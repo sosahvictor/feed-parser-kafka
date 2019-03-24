9ac2cc01cd7f11892d1c521f742da5d095916627
Francisco Vargas <fvargasp@akamai.com>
2019-03-23 22:22:49 -0600

    Add comments to kafka parser and change topic from test to rss

--- feed-parser-kafka/kafka_producer.py
+++ feed-parser-kafka/kafka_producer.py
@@ -14,19 +14,23 @@ ns = {
 
 # Parse and publish all feeds from RSS feed XML files list
 def process_feed_file(files):
+	# Iterate over each XML file and publish
 	for feed_file in files:
 		parse_xml_feed(feed_file)
 		
-
+# Parse independent RSS feed XML files and publish the feeds
 def parse_xml_feed(feed_file):
 	print("Pushing feed file: " + feed_file)
 	tree = ET.parse(feed_file)
 	channel_element = tree.getroot()[0]
 
+	# Iterate over the RSS feed items which represents each article or post
 	for feed_item in channel_element.iter('item'):
+		# Extract author, description, title and URL of each article or post
 		author = feed_item.find('dc:creator',ns).text if feed_item.find('dc:creator', ns) is not None else feed_item.find('author').text
 		url = feed_item.find('atom:link', ns).attrib['href'] if feed_item.find('atom:link', ns) is not None else feed_item.find('link').text
 
+		# Format them as JSON before publishing
 		feed = {
 			"title" : feed_item.find('title').text,
 			"author" : author,
@@ -36,7 +40,10 @@ def parse_xml_feed(feed_file):
 		json_data = json.dumps(feed)
 		now = datetime.datetime.now()
 		timestamp = now.strftime("%Y%m%d%H%M%S%f")
-		producer.produce(topic= 'test', key=timestamp, value=json_data)
 
+		# Publish
+		producer.produce(topic= 'rss', key=timestamp, value=json_data)
+
+	# Clean producer object
 	producer.flush()
 
