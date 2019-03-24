f16e3a7810a75347e8b6b2dcd53d137e7b1637e9
Francisco Vargas <fvargasp@akamai.com>
2019-03-24 10:40:11 -0600

    Add comments to main and check tralinig slash on output directory

--- feed-parser-kafka/main.py
+++ feed-parser-kafka/main.py
@@ -6,6 +6,8 @@ import file_comparator
 import kafka_producer
 import argparse
 
+# Two arguments are expected, the file with the list of RSS feed URLs and the output directory
+# to store downloaded RSS feed XML files.
 argparser = argparse.ArgumentParser(description = 'Download, parse, and publish RSS feeds to Kafka')
 argparser.add_argument("rss_url_file",
                         help="Path to file with list of RSS feed URLs to connect to.", type=str)
@@ -13,8 +15,13 @@ argparser.add_argument("output_directory",
                        help="Path to directory to store downloaded RSS feed XML files.", type=str)
 args = argparser.parse_args()
 
+# Save arguments in variables
 rss_file_location = args.rss_url_file
 output_directory = args.output_directory
+
+# Add trailing slash if output directory doesn't have it
+if output_directory.endswith('/') is False:
+	output_directory = output_directory + "/"
 
 # Read, download and store RSS feed XMLs based on list of RSS feed URLs
 feed_retriever.read_rss_file(rss_file_location, output_directory)
